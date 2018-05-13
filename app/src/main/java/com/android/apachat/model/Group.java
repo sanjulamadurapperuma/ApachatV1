59a7d3df60a7d2e6eff5f4822395c90bc14029df
Sanjula Madurapperuma <32578617+sanjulamadurapperuma@users.noreply.github.com>
2018-05-13 16:57:10 +0530

    Initial commit

---
+++ app/src/main/java/com/android/apachat/model/Group.java
@@ -0,0 +1,16 @@
+package com.android.apachat.model;
+
+
+
+public class Group extends Room{
+    public String id;
+    public ListFriend listFriend;
+
+    public Group(){
+        listFriend = new ListFriend();
+    }
+
+    public ListFriend getListFriend() {
+        return listFriend;
+    }
+}
